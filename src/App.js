1704eb7bac5b69b0653e7c34151e37e901fbc50e
pallavi1812 <upallavi1812@gmail.com>
2020-11-10 14:44:20 +0530

    recipe website

--- src/App.js
+++ src/App.js
@@ -1,60 +1,81 @@
-import React,{ useEffect, useState } from 'react';
-import Recipes from './Recipes';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Recipes from "./Recipes";
+import {
+  Form,
+  FormGroup,
+  Input
+} from "reactstrap";
+import "./App.css";
 
 function App() {
-
   const AppId = "704d26a2";
   const AppKey = "8ec8898a85b792657a15a980f909c748";
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
-  const [query, setQuery] = useState("chicken");
+  const [query, setQuery] = useState("almond");
 
- useEffect(() => {
-     getRecipes();
- },[query]);
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
 
- const getRecipes = async () => {
-   const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${AppId}&app_key=${AppKey}&from=0&to=3&calories=591-722&health=alcohol-free`);
-   const data = await response.json();
-   setRecipes(data.hits);
-   console.log(data.hits);
- };
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${AppId}&app_key=${AppKey}&from=0&to=3&calories=591-722&health=alcohol-free`
+    );
+    const data = await response.json();
+    setRecipes(data.hits);
 
- const changeSearch = (e) => {
-   setSearch(e.target.value);
- };
+    console.log(data.hits);
+  };
 
- const getSearch = (e) => {
-  e.preventDefault();
-  setQuery(search);
-  setSearch('');
-};
+  const changeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
 
   return (
-    <div className="App">
-      <p>Enter any food item to get recipes related to that item!!</p>
-      <form onSubmit={getSearch} className="searchForm">
-        <input
-          type="text"
-          name="search"
-          id="search"
-          className="searchBar"
-          value={search}
-          onChange={changeSearch}
-        />
-        <button className="searchButton" type="submit">
-          Search
-        </button>
-      </form>
+    <div className="app">
+      <div className="header">
+        <h2 className="fline" >Enter any food item to get some recipes!</h2>
+      </div>
+      <div className="container">
+      <Form onSubmit={getSearch} className="searchForm">
+        <FormGroup>
+          <div className="row">
+            <Input
+              type="text"
+              name="search"
+              id="search"
+              className="form-control col-10"
+              placeholder="Search.."
+              value={search}
+              onChange={changeSearch}
+            />
+            <Input type="submit" className="col-1 ml-1 btn btn-primary" />
+          </div>
+        </FormGroup>
+      </Form>
       {recipes.map((recipe) => (
-          <Recipes 
+        <Recipes
           key={recipe.recipe.label}
           name={recipe.recipe.label}
+          ingd={recipe.recipe.ingredientLines.join()}
+          caution={recipe.recipe.cautions.join()}
+          HealthL={recipe.recipe.healthLabels.join()}
           calories={recipe.recipe.calories.toFixed(2)}
-          image={recipe.recipe.image} />
+          image={recipe.recipe.image}
+        />
       ))}
+    </div>
+    <div className="footer">
+      Coded by : <a href="https://github.com/pallavi-1812"> Pallavi Upadhyay</a>
+    </div>
     </div>
   );
 }
--- src/Recipes.js
+++ src/Recipes.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React from "react";
+import { Card,CardBody,CardText,CardTitle,CardSubtitle,CardImg} from "reactstrap";
 
-const Recipes = ({name,calories,image}) => {
-    return(
-        <div>
-            <h2>Title: <span>{name}</span> </h2>
-            <h2>Calories: <span>{calories} &nbsp; kcal</span> </h2>
-            <img alt={name} src={image} />
-        </div>
-    );
-}
+const Recipes = ({ name, calories, image, ingd, caution, HealthL }) => {
+    if(caution===''){
+        caution='None';
+    }
+  return (
+    <div className="container">
+      <div className="row">
+      <Card className="col-7 offset-2 mb-4">
+      <CardImg top alt={name} src={image} className="mt-3"/>
+      <CardBody>
+      <CardTitle tag="h3"> {name} </CardTitle>
+      <CardSubtitle tag="h6" className="mb-2 text-muted">
+        Calories : {calories} Kcal
+      </CardSubtitle>
+      <CardText>
+        <p> Ingredients : {ingd} </p>
+        <p> Caution : {caution} </p>
+        <p> Health Labels : {HealthL} </p>
+      </CardText>
+      </CardBody>
+      </Card>
+      </div>
+    </div>
+  );
+};
 
 export default Recipes;
